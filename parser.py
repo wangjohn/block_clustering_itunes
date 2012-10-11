8c88ff1f1084bb9d35ab6f2f0c3d7f83cd258df4
John <john@ubuntu.(none)>
2012-10-11 17:52:12 -0400

    Made the title score keep track of inline.

--- parser.py
+++ parser.py
@@ -1,8 +1,11 @@
+from parameters import * 
+
 class Fitness:
-    def __init__(self, description):
+    def __init__(self, description, params = Parameters()):
         lines = description.split('\n')
         self.text = lines
 	    self.potential_blocks = {}
+        self.parameters = params
 
     def check_first_n_letters(self, n):
         first_letters = {}
@@ -63,15 +66,14 @@ class Fitness:
         title_indices = []          # get the line indices of the probable titles
         double_line_breaks = []    # get the line indices of where \n\n happens
         for i in xrange(num_lines):
+            # if this is a title, append it to the list
             score = self.check_if_title(self.text[i])
-            if score >= self.parameters.title_score_threshold:
-                # if this is a title, append it to the list
-                title_indices.append(i)
-            if i > 1:
-                # if this is a double line break, append it to the list
-                result = self.check_double_line_break(self.text[i])
-                if result:
-                    double_line_breaks.append(i)
+            title_indices.append((i, score))
+
+            # if this is a double line break, append it to the list
+            result = self.check_double_line_break(self.text[i])
+            if result:
+                double_line_breaks.append(i)
 
         # we will assume that a block starts at the first line.
         for i in xrange(len(double_line_breaks)):
@@ -91,14 +93,15 @@ class Fitness:
         block_indices = blocks_hash.keys.sort()
         block_counter = 0
         current_block = block_indices[block_counter]
-        for title_position in title_indices:
+        for (title_position, title_score) in title_indices:
             while title_position > current_block[1]:
                 block_counter += 1
                 current_block = block_indices[block_counter]
             if title_position <= current_block[1] and title_position >= current_block[0]:
-                score = float(current_block[1] - title_position) / current_block[0]
+                # basically, we scale the score by how close the title is to the top of the block.
+                score = title_score*(1 - float(current_block[1] - title_position) / current_block[0])
 		block_object = blocks_hash[current_block]
-		block_object.set_subscore('title', self.parameters.title_score)
+		block_object.set_subscore('title', score)
 
 
 class Block:
@@ -121,8 +124,15 @@ class Block:
 
 	def recompute_score(self):
 		self.score = self.double_line_break_score + self.title_score	
+
+        # if the block has both list and url elements, then something went wrong and we should penalize for that
 		if self.list_score and self.url_score:
 			self.score -= self.parameters.list_url_collision_penalty 
+        else:
+            if self.list_score:
+                self.score += self.list_score
+            if self.url_score:
+                self.score += self.url_score
 	
 	def set_subscore(self, score_type, score):
 		eval(self.score_hash[score_type] + "=" + str(score))
